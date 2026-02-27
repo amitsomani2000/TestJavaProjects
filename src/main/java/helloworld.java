924e7fcf6331354f366ed3f48aa37578dbbc7da6
Amit Somani <amitsomani2000@gmail.com>
2026-02-27 18:32:40 +0530

    New Method Added in Files

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -3,5 +3,11 @@ package main.java;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello World from Amit ");
+        System.out.println("This is the output from :" +  getCount());
     }
+
+    private static double getCount(){
+        return Math.random() * 10000 ;
+    };
 }
+
--- src/main/test/helloworldtest.java
+++ src/main/test/helloworldtest.java
@@ -1,5 +1,10 @@
 public class helloworldtest {
     public static void main(String[] args) {
         System.out.println("I m in Test Java");
+        testRandom();
+    }
+
+    private static void testRandom (){
+        System.out.println(" I am in testRandom");
     }
 }
