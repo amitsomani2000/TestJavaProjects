4acc155b3bf1888f5046992cac3c171e78d65d21
Amit Somani <amitsomani2000@gmail.com>
2026-02-27 22:48:53 +0530

    Phase 1 getPhase Changes

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -4,11 +4,16 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello World from Amit ");
         System.out.println("This is the output from :" +  getCount());
+        System.out.println("=================");
+        System.out.println("I am Currently Phase 1: " + getPhase()); 
     }
 
     private static double getCount(){
         return Math.random() * 10000 ;
     };
 
+    private static String getPhase(){
+        return "phase1";
+    }
 }
 
